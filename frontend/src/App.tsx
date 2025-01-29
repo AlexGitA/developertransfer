c39fe487207d6abf83f6b89c04ad1d7e598778f9
twoichai <ibenyuk@gmail.com>
2025-01-29 11:47:18 +0100

    ment-036: added routing and removed the root styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,19 +4,21 @@ import LoginPage from './pages/auth/LoginPage'
 import Layout from "@/layout/Layout.tsx";
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import DerPlayground from "@/playground/DerPlayground.tsx";
+import ProfilePage from "@/pages/profile/ProfilePage.tsx";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="login" element={<LoginPage />} />
-            <Route path="playground" element={<DerPlayground />} />
-          {/* Add more routes as needed */}
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  )
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout/>}>
+                    <Route path="login" element={<LoginPage/>}/>
+                    <Route path="playground" element={<DerPlayground/>}/>
+                    <Route path="profile/:id" element={<ProfilePage/>}/>
+                    {/* Add more routes as needed */}
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App
