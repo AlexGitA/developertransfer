a490fda085866dd4cd3e9e0dce95bb29f1d20b4a
twoichai <ibenyuk@gmail.com>
2025-01-29 23:53:44 +0100

    Cleaned code, adjusted README for better use, added home element for later use, added flag component, adjusted Header.tsx, created routing with header

--- frontend/src/layout/Header/Header.tsx
+++ frontend/src/layout/Header/Header.tsx
@@ -1,19 +1,32 @@
 import "../../index.css";
 import { MENTButton } from "@/components/button/MENT-button.tsx";
 import ProfileButton from "@/layout/Header/profileButton.tsx";
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  const handleLogoClick = () => {
+    navigate('/home');
+  };
+
   return (
-    <header className="main-header">
+    <header className="main-header h-14">
       <div className="main-header__left">
-        <h1 className="main-header__left-logo">
-            <text className="main-header__left-text">Mentor<span className="main-header__left-text-space">X</span></text>
+        <h1
+          className="main-header__left-logo"
+          onClick={handleLogoClick}
+          style={{ cursor: 'pointer' }} // Add this to show it's clickable
+        >
+          <text className="main-header__left-text">
+            Mentor<span className="main-header__left-text-space">X</span>
+          </text>
         </h1>
       </div>
       <div className="main-header__mid text-center">
-          <MENTButton className="main-header__mid-button">
-              <span className="main-header__mid-button-text">Suche deinen Mentor   🔍</span>
-          </MENTButton>
+        <MENTButton className="main-header__mid-button">
+          <span className="main-header__mid-button-text">Suche deinen Mentor   🔍</span>
+        </MENTButton>
       </div>
       <div className="main-header__right">
         <ProfileButton/>
@@ -21,6 +34,5 @@ const Header = () => {
     </header>
   );
 };
-
 
 export default Header;
