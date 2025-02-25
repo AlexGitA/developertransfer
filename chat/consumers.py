4fafa00ef6880040a8b34d842f12617480402135
Alexander Amen <alexander.amen@telekom.de>
2025-02-25 11:56:12 +0100

    [MENT-42] fixed small issue

--- chat/consumers.py
+++ chat/consumers.py
@@ -7,40 +7,39 @@ class ChatConsumer(AsyncWebsocketConsumer):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = f'chat_{self.room_name}'
 
-        # Join room group
+        # Join the room group
         await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
         )
         await self.accept()
+        print(f"Client {self.channel_name} connected to room {self.room_group_name}")
 
     async def disconnect(self, close_code):
-        # Leave room group
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
+        print(f"Client {self.channel_name} disconnected from room {self.room_group_name}")
 
-    # Receive message from WebSocket
     async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
+        data = json.loads(text_data)
+        message = data.get("message")
+        sender = data.get("sender")
+        print(f"Received message: {message} from sender: {sender}")
+        if message:
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'chat_message',
+                    'message': message,
+                    'sender': sender,
+                }
+            )
 
-        # Send message to room group
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                'type': 'chat_message',
-                'message': message,
-                'sender': sender,
-            }
-        )
-
-    # Receive message from room group
     async def chat_message(self, event):
         message = event['message']
-
-        # Send message to WebSocket
+        sender = event['sender']
         await self.send(text_data=json.dumps({
             'message': message,
             'sender': sender,
